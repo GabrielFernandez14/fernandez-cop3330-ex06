9d3bde438a81a3293cba54c6d19fb5b38a181a0f
GabrielFernandez14 <79731548+GabrielFernandez14@users.noreply.github.com>
2021-06-04 13:06:48 -0400

    Update App.java

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,7 +1,6 @@
-/* Gabriel Fernandez
- * COP3330 - Summer CV01
- * "Motivated" Practice Exercises
- * Exercise 06 - Retirement Calculator
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solutions
+ *  Copyright 2021 Gabriel Fernandez
  */
 
 package org.example;
